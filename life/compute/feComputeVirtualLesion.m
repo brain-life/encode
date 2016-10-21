44bce77221bc4d4d8e11459577e71d4f6fd3dac6
Franco Pestilli <frakkopesto@gmail.com>
2016-10-21 15:27:05 -0400

    Update feComputeVirtualLesion.m

--- life/compute/feComputeVirtualLesion.m
+++ life/compute/feComputeVirtualLesion.m
@@ -1,11 +1,14 @@
+function [ rmse_wVL, rmse_woVL, nFib_tract, nFib_PN, nVoxels] = feComputeVirtualLesion(fe, ind_tract)
 % This function compute the rmse in a path neighborhood voxels with and
 % without Virtual Lesion
-%  Copyright (2015), Franco Pestilli (Indiana Univ.) - Cesar F. Caiafa (CONICET)
-%  email: pestillifranco@gmail.com and ccaiafa@gmail.com
-function [ rmse_wVL, rmse_woVL, nFib_tract, nFib_PN, nVoxels] = feComputeVirtualLesion(fe, ind_tract)
+%
+%
 % INPUTS:
 % fe: fe structure
 % ind1: indices to fibers in the tract to be virtually lesioned
+%
+%  Copyright (2016), Franco Pestilli (Indiana University) - Cesar F. Caiafa (CONICET)
+%  email: frakkopesto@gmail.com and ccaiafa@gmail.com
 
 % ind_nnz = find(fe.life.fit.weights);
 % ind_tract = ind_nnz(ind1);
